52a3da3ca4577898a528926da877627dfbcf1f4b
Kauan <raidoka546@gmail.com>
2025-10-29 11:33:07 -0400

    Inital commit

---
+++ src/App.vue
@@ -0,0 +1,6 @@
+<script setup>
+</script>
+
+<template>
+  <h1 class="text-gray-500 text-3xl font-bold">Eu sabo</h1>
+</template>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import '../src/index.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
