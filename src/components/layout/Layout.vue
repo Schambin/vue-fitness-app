6d47cbf1e15dd3fbb8f766e9a23962802aaf0491
Kauan <raidoka546@gmail.com>
2025-11-05 12:22:00 -0400

    added an github icon

--- src/components/layout/Layout.vue
+++ src/components/layout/Layout.vue
@@ -14,6 +14,7 @@
         <a href="https://github.com/Schambin" target="_blank" class="flex items-center gap-2 p-[0.3rem] pr-2 bg-background-muted rounded-[4rem] border-2 border-transparent border-solid duration-200 no-underline hover:border-color-link"> 
             <img src="https://avatars.githubusercontent.com/u/118319638?v=4" alt="pfp" class="max-w-[30px] aspect-square rounded-full">
             <p>Kauan Schamber</p>
+            <i class="fa-brands fa-github"></i>
         </a>
     </footer>
 </template>
