937ca840f8041a129b6de40a371978105ee9fc8c
Kauan <raidoka546@gmail.com>
2025-11-05 12:23:06 -0400

    created dashboard card

---
+++ src/components/pages/Dashboard.vue
@@ -0,0 +1,23 @@
+<script setup>
+import Grid from '../Grid.vue'
+import  { gymHealthFacts } from '../../utils'
+
+const randomNumber = Math.floor(Math.random() * gymHealthFacts.length);
+const todaysFact = gymHealthFacts[randomNumber];
+</script>
+
+<template>
+    <!-- Dashboard -->
+    <section>
+        <!-- card tip container -->
+        <div class="card">
+            <h2>Bem-Vindo Soldado(a)</h2>
+            <div>
+                <!-- tip -->
+                <p><strong>Dica Diária</strong><br> {{ todaysFact }}</p>
+            </div>
+            <button>Começar Treino &rarr;</button>
+        </div>
+        <Grid />
+    </section>
+</template>
