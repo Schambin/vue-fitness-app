c939a9c1968bcbef964b77af32b29c16462da163
Kauan Arthur <raidoka546@gmail.com>
2025-11-11 16:04:34 -0300

    finished welcome page

--- src/components/pages/Welcome.vue
+++ src/components/pages/Welcome.vue
@@ -1,28 +1,29 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
-  <section class="flex flex-col gap-6 sm:gap-8 sm:py-4">
-    <div class="flex flex-col gap-2 sm:gap-2">
-        <h2>Complete este programa <br/>se você quiser...</h2>
-        <div>
-            <p>✅ Seguir um programa simples com resultados verdadeiros.</p>
-            <p>✅ Ficar em forma, saudável, forte e absolutamente fibrado(a).</p>
-            <p>✅ Aprender sobre academia, treino e técnica.</p>
+    <section class="flex flex-col gap-6 sm:gap-8 sm:py-4">
+        <div class="flex flex-col gap-2 sm:gap-2">
+            <h2>Complete este programa <br />se você quiser...</h2>
+            <div>
+                <p>✅ Seguir um programa simples com resultados verdadeiros.</p>
+                <p>
+                    ✅ Ficar em forma, saudável, forte e absolutamente
+                    fibrado(a).
+                </p>
+                <p>✅ Aprender sobre academia, treino e técnica.</p>
+            </div>
+            <div>
+                <h3>O plano de treino</h3>
+                <p>Este programa segue uma estrutura conhecida como:</p>
+                <strong>Bro Split</strong>
+                <h5><i>Push &rarr; Pull &rarr; Legs &rarr; Repeat</i></h5>
+            </div>
+            <div class="card flex flex-col gap-1">
+                <h3>🔥 Seu Sesafio</h3>
+                <p>Complete todos os treinos e acompanhe seu progresso!</p>
+                <p>Você aceita?</p>
+                <button>Começar &rarr;</button>
+            </div>
         </div>
-        <div>
-            <h3>O plano de treino</h3>
-            <p>Este programa segue uma estrutura conhecida como: </p>
-            <strong>Bro Split</strong>
-            <h5><i>Push &rarr; Pull &rarr; Legs &rarr; Repeat</i></h5>
-        </div>
-        <div class="card flex flex-col gap-1">
-            <h3>🔥 Seu Sesafio</h3>
-            <p>Complete todos os treinos e acompanhe seu progresso!</p>
-            <p>Você aceita?</p>
-            <button>Começar &rarr;</button>
-        </div>
-    </div>
-  </section>
-</template> 
+    </section>
+</template>
