eee96b9ce1a7fa8c965e02e5fb8eea706a63c184
Kauan Arthur <raidoka546@gmail.com>
2025-11-11 15:59:15 -0300

    imported main pages

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <script setup>
-import Welcome from './components/pages/Welcome.vue';
-import Layout from './components/layout/Layout.vue';
-import Dashboard from './components/pages/Dashboard.vue'
-import Workout from './components/pages/Workout.vue'
+import Welcome from "./components/pages/Welcome.vue";
+import Layout from "./components/layout/Layout.vue";
+import Dashboard from "./components/pages/Dashboard.vue";
+import Workout from "./components/pages/Workout.vue";
 
 const selectedDisplay = 2;
 </script>
 
 <template>
-  <Layout>
-    <!-- PAGE 1 -->
-    <Welcome v-if="selectedDisplay == 1"/>
-    <!-- PAGE 2 -->
-    <Dashboard v-if="selectedDisplay == 2"/>
-    <!-- PAGE 3 -->
-    <Workout v-if="selectedDisplay == 3"/>
-  </Layout>
+    <Layout>
+        <!-- PAGE 1 -->
+        <Welcome v-if="selectedDisplay == 1" />
+        <!-- PAGE 2 -->
+        <Dashboard v-if="selectedDisplay == 2" />
+        <!-- PAGE 3 -->
+        <Workout v-if="selectedDisplay == 3" />
+    </Layout>
 </template>
