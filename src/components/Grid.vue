49a1a8ff5e198e252d36f66190a066e4dff07714
Kauan Arthur <raidoka546@gmail.com>
2026-01-15 16:23:09 -0300

    updated Grid component to use static workoutTypes array, added firstIncompleteWorkoutIndex and handleResetPlan props and improved button disable logic for workout selection and reset functionality

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -1,23 +1,26 @@
 <script setup>
 import { workoutProgram } from "../utils";
-import { ref } from "vue";
 
 defineProps({
     handleSelectedWorkout: Function,
+    firstIncompleteWorkoutIndex: Number,
+    handleResetPlan: Function,
 });
 
-const workoutTypes = ref(["Push", "Pull", "Legs"]);
+const workoutTypes = ["Push", "Pull", "Legs"];
+
+
 </script>
 
 <template>
     <!-- grid -->
-    <section class="grid grid-cols-3 gap-4 sm:gridgrid-cols-4">
+    <section class="grid grid-cols-3 gap-4 sm:grid-cols-4">
         <button
-            disabled="true"
+            :disabled="workoutIdx > 0 && workoutIdx > firstIncompleteWorkoutIndex"
             @click="handleSelectedWorkout(workoutIdx)"
             :key="workoutIdx"
             v-for="(workout, workoutIdx) in Object.keys(workoutProgram)"
-            class="card-button plan-card w-full flex flex-col disabled:shadow-none"
+            class="card-button plan-card w-full flex flex-col disabled:shadow-none disabled:cursor-not-allowed"
         >
             <div class="flex items-center justify-between gap-2 ">
                 <p class="text-left">
@@ -30,7 +33,14 @@ const workoutTypes = ref(["Push", "Pull", "Legs"]);
                 <i class="fa-solid fa-weight-hanging" v-if="workoutIdx % 3 == 1"></i>
                 <i class="fa-solid fa-bolt" v-if="workoutIdx % 3 == 2"></i>
             </div>
-            <h3>{{workoutTypes[workoutIdx % 3]}}</h3>
+            <h3 class="text-2xl">{{workoutTypes[workoutIdx % 3]}}</h3>
         </button>
     </section>
+    <button 
+        :disabled="firstIncompleteWorkoutIndex != -1"
+        @click="handleResetPlan"
+        class="card-button plan-card flex w-full items-center justify-center gap-4">
+        <p>Reset</p>
+        <i class="fa-solid fa-rotate-left"></i>
+    </button>
 </template>
