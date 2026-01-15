7079810b3605744601aec7bc0509e74ca71fb387
Kauan Arthur <raidoka546@gmail.com>
2026-01-15 16:22:37 -0300

    refactored workout completion logic, added firstIncompleteWorkoutIndex computed property, and implemented handleResetPlan function; updated template to pass new props to Dashboard and Workout components

--- src/App.vue
+++ src/App.vue
@@ -5,22 +5,38 @@ import Dashboard from "./components/pages/Dashboard.vue";
 import Workout from "./components/pages/Workout.vue";
 
 import { workoutProgram } from './utils'
-import { computed, ref } from "vue";
+import { computed, onMounted, ref } from "vue";
 
 const defaultData = {};
 const selectedDisplay = ref(1);
 const data = ref(defaultData);
 const selectedWorkout = ref(-1);
 
-const isWorkoutComplete = computed(() => {
+const isWorkoutCompleted = computed(() => {
     const currWorkout = data.value?.[selectedWorkout.value]
-    if(!currWorkout) return false;
+    if(!currWorkout) {
+        return false;
+    }
 
     const isCompleteCheck = Object.values(currWorkout).every(ex => !!ex);
     return isCompleteCheck;
 });
 
+const firstIncompleteWorkoutIndex = computed(() => {
+    const allWorkouts = data.value;
+    if(!allWorkouts) {
+        return -1;
+    }
 
+    for(const [index, workout] of Object.entries(allWorkouts)) {
+        const isComplete = Object.values(workout).every(ex => !!ex);
+        if(!isComplete) {
+            return parseInt(index);
+        }
+    };
+
+    return -1;
+});
 
 for(let workoutIdx in workoutProgram) {
     const workoutData = workoutProgram[workoutIdx];
@@ -33,6 +49,7 @@ for(let workoutIdx in workoutProgram) {
 
 function handleChangeDisplay(idx) {
     selectedDisplay.value = idx;
+    selectedWorkout.value = -1;
 }
 
 function handleSelectedWorkout(idx) {
@@ -43,17 +60,53 @@ function handleSelectedWorkout(idx) {
 function handleSaveWorkout() {
     localStorage.setItem('workouts',JSON.stringify(data.value));
     selectedDisplay.value = 2;
-    selectedWorkout.value = -1
+    selectedWorkout.value = -1;
 }
+
+function handleResetPlan() {
+    selectedDisplay.value = 2;
+    selectedWorkout = -1;
+    data.value = defaultData;
+    localStorage.removeItem('workouts');
+}
+
+onMounted(() => {
+    console.log('pagina carregou');
+
+    if(!localStorage) return;
+
+    if(localStorage.getItem('workouts')) {
+        const savedData = JSON.parse(localStorage.getItem('workouts'));
+        data.value = savedData;
+        selectedDisplay.value = 2; 
+    }
+});
 </script>
 
 <template>
-    <Layout>
-        <!-- PAGE 1 -->
-        <Welcome :handleChangeDisplay="handleChangeDisplay" v-if="selectedDisplay == 1" />
-        <!-- PAGE 2 -->
-        <Dashboard :handleSelectedWorkout="handleSelectedWorkout" v-if="selectedDisplay == 2" />
-        <!-- PAGE 3 -->
-        <Workout :data="data" :selectedWorkout="selectedWorkout" v-if="workoutProgram?.[selectedWorkout]" />
-    </Layout>
+        <Layout
+            :handleChangeDisplay="handleChangeDisplay"
+        >
+            <!-- PAGE 1 -->
+            <Welcome
+                v-if="selectedDisplay == 1"
+                :handleChangeDisplay="handleChangeDisplay"
+            />
+        
+            <!-- PAGE 2 -->
+            <Dashboard
+                v-if="selectedDisplay == 2"             
+                :firstIncompleteWorkoutIndex="firstIncompleteWorkoutIndex"   
+                :handleSelectedWorkout="handleSelectedWorkout"
+                :handleResetPlan="handleResetPlan"
+            />
+            <!-- PAGE 3 -->
+            <Workout
+                v-if="workoutProgram?.[selectedWorkout]"
+                :data="data"
+                :selectedWorkout="selectedWorkout"
+                :isWorkoutCompleted="isWorkoutCompleted"
+                :handleSaveWorkout="handleSaveWorkout"
+            />
+        </Layout>
 </template>
